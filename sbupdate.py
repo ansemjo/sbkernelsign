ea9bf56feb6196fdd647f41183bcf31d01de6499
Anton Semjonov <anton@semjonov.de>
2018-10-13 03:00:22 +0200

    updated config structure with global

--- sbupdate.py
+++ sbupdate.py
@@ -4,75 +4,92 @@ import subprocess
 import configparser
 import argparse
 import shutil
+import glob
 import sys
 import os
 
-config = configparser.ConfigParser(
-  interpolation=configparser.ExtendedInterpolation())
+import chalk
 
-# string constants for variable names
-NAME, BOOT, EFISTUB = 'name', 'boot', 'efistub'
-KEY, CERT, KERNEL = 'key', 'crt', 'kernel'
-INITRAMFS, CMDLINE = 'initramfs', 'cmdline'
-OUTPUT = 'output'
+# .ini config parser with ${var} interpolation
+config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 
-OBJCOPY_SCRIPT = '/home/ansemjo/script/sbupdate-py/efistub-combine.sh'
+# configuration variable names
+NAME = "name"
+BOOT = "boot"
+EFISTUB = "efistub"
+KEY = "key"
+CERT = "crt"
+KERNEL = "kernel"
+INITRAMFS = "initramfs"
+CMDLINE = "cmdline"
+OUTPUT = "output"
 
-# todo: accept commandline parameter
-with open('secureboot.ini') as ini:
-  config.read_file(ini)
+# TODO: put into config
+OBJCOPY_SCRIPT = "/home/ansemjo/git/script/sbupdate/efistub-combine.sh"
+
+# TODO: commandline parser
+with open("secureboot.ini") as ini:
+    config.read_file(ini)
+
+# add missing sections by globbing
+kernelglob = config.get("global", "kernels", fallback="/boot/vmlinuz-*")
+for kernel in (k[len(kernelglob) - 1 :] for k in glob.iglob(kernelglob)):
+    if not config.has_section(kernel):
+        config.add_section(kernel)
 
 # check configuration
-for s in config.sections():
+for s in [s for s in config.sections() if s != "global"]:
 
-  # set 'name' to section name by default
-  if not config.has_option(s, NAME):
-    config.set(s, NAME, s)
+    # set 'name' to section name by default
+    if not config.has_option(s, NAME):
+        config.set(s, NAME, s)
 
-  # debugging output
-  if False:
-    print(f'[{s}]')
-    for i in config.items(s): print(i)
-    print()
+    # debugging output
+    if True:
+        print(f"[{s}]")
+        for i in config.items(s):
+            print(i)
+        print()
 
-  # check if everything that is required is present
-  for r in [EFISTUB, KEY, CERT, KERNEL, INITRAMFS, CMDLINE, OUTPUT]:
-    if not config.has_option(s, r):
-      raise ValueError(f'required option {r} is missing in section {s}')
+    # check if everything that is required is present
+    for r in [EFISTUB, KEY, CERT, KERNEL, INITRAMFS, CMDLINE, OUTPUT]:
+        if not config.has_option(s, r):
+            raise ValueError(f"required option {r} is missing in section {s}")
 
 # combine efistub, kernel and initramfs into a single binary
-def objcopy (efistub, kernel, initramfs, cmdline, output):
-  cmd = [OBJCOPY_SCRIPT, '-v', '-e', efistub, '-k', kernel, '-c', cmdline, '-o', output]
-  for image in initramfs:
-    cmd.extend(['-i', image])
-  return subprocess.run(cmd, check=True)
+def objcopy(efistub, kernel, initramfs, cmdline, output):
+    cmd = [OBJCOPY_SCRIPT, "-v", "-e", efistub, "-k", kernel, "-c", cmdline, "-o", output]
+    for image in initramfs:
+        cmd.extend(["-i", image])
+    return subprocess.run(cmd, check=True)
+
 
 # create a signature on the efistub binary
-def sbsign (key, cert, binary):
-  cmd = ['sbsign', '--key', key, '--cert', cert, '--output', binary, binary]
-  return subprocess.run(cmd, check=True)
+def sbsign(key, cert, binary):
+    cmd = ["sbsign", "--key", key, "--cert", cert, "--output", binary, binary]
+    return subprocess.run(cmd, check=True)
+
 
 # let's go
-for cfg in [config[s] for s in config.sections()]:
+for cfg in [config[s] for s in config.sections() if s != "global"]:
 
-  # fetch values
-  efistub, cmdline, output = cfg[EFISTUB], cfg[CMDLINE], cfg[OUTPUT]
-  kernel, key, cert = cfg[KERNEL], cfg[KEY], cfg[CERT]
-  # parse initramfs list
-  initramfs = cfg[INITRAMFS].strip().splitlines()
+    # fetch values
+    efistub, cmdline, output = cfg[EFISTUB], cfg[CMDLINE], cfg[OUTPUT]
+    kernel, key, cert = cfg[KERNEL], cfg[KEY], cfg[CERT]
+    name = cfg[NAME]
+    # parse initramfs list
+    initramfs = cfg[INITRAMFS].strip().splitlines()
 
-  # if a 'boot' config option exists, join paths
-  if BOOT in cfg:
-    kernel = os.path.join(cfg[BOOT], kernel)
-    initramfs = [os.path.join(cfg[BOOT], image) for image in initramfs]
+    # print info about this binary
+    print(chalk.bold(f">>> {name}"))
+    print("kernel  :", chalk.magenta(kernel))
+    print("initrd  :", chalk.green(initramfs))
+    print("cmdline :", chalk.cyan(cmdline))
 
-  # print info about this binary
-  print((efistub, kernel, initramfs, cmdline, output))
+    # create backup copy
+    if os.path.isfile(output):
+        shutil.copy2(output, output + ".bak")
 
-  # create backup copy
-  if os.path.isfile(output):
-    shutil.copy2(output, output+'.bak')
-
-  # assemble and sign binary
-  objcopy(efistub, kernel, initramfs, cmdline, output)
-  sbsign(key, cert, output)
+    # assemble and sign binary
+    objcopy(efistub, kernel, initramfs, cmdline, output)
+    sbsign(key, cert, output)
