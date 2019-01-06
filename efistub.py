2967a914dbf1f3dec817942d2504bee1650b3c32
Anton Semjonov <anton@semjonov.de>
2019-01-06 22:39:59 +0100

    add argumentparser to efistub.py

--- efistub.py
+++ efistub.py
@@ -3,26 +3,30 @@
 
 import subprocess
 import threading
-import magic  #< req
-import distro #< req
+import argparse
+import magic  # < req
+import distro  # < req
 import re
 import os
 
-OSREL = '/etc/os-release'
-EFISTUB = '/usr/lib/systemd/boot/efi/linuxx64.efi.stub'
-INITRAMFS = ['/boot/intel-ucode.img', '/boot/initramfs-linux.img']
-KERNEL = '/boot/vmlinuz-linux'
-CMDLINE = 'noop'
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", dest="efistub", help="efi stub", default="/usr/lib/systemd/boot/efi/linuxx64.efi.stub")
+parser.add_argument("-k", dest="kernel", help="linux kernel", required=True)
+parser.add_argument("-i", dest="initramfs", action="append", help="initramfs image", default=[])
+parser.add_argument("-c", dest="cmdline", help="kernel commandline", default="")
+parser.add_argument("-o", dest="output", help="output file", required=True)
+args = parser.parse_args()
+print(args)
 
 # concatenate initramfs in pipe
 initramfs_r, initramfs_w = os.pipe()
 initramfs_w = os.fdopen(initramfs_w)
-iniramfs_cat = subprocess.Popen(['cat'] + INITRAMFS, stdout=initramfs_w)
+iniramfs_p = subprocess.Popen(["cat"] + args.initramfs, stdout=initramfs_w)
 
 # write cmdline through pipe
 cmdline_r, cmdline_w = os.pipe()
 def pipe_cmdline():
-  os.write(cmdline_w, CMDLINE.encode())
+  os.write(cmdline_w, args.cmdline.encode())
   os.close(cmdline_w)
 cmdline_t = threading.Thread(target=pipe_cmdline)
 cmdline_t.start()
@@ -30,33 +34,33 @@ cmdline_t.start()
 # write dynamic osrelease info through pipe
 osrel_r, osrel_w = os.pipe()
 def pipe_osrelease():
-  kernelver = re.sub(r'.*version ([^ ,]+).*', r'\1', magic.from_file(KERNEL))
-  os.write(osrel_w, f'NAME="{distro.name()}"\nID="{os.path.basename(KERNEL)}"\nVERSION_ID="{kernelver}"\n'.encode())
+  kernelver = re.sub(r".*version ([^ ,]+).*", r"\1", magic.from_file(args.kernel))
+  os.write(osrel_w, f'NAME="{distro.name()}"\n'.encode())
+  os.write(osrel_w, f'ID="{os.path.basename(args.kernel)}"\n'.encode())
+  os.write(osrel_w, f'VERSION_ID="{kernelver}"\n'.encode())
   os.close(osrel_w)
 osrel_t = threading.Thread(target=pipe_osrelease)
 osrel_t.start()
 
 # combine with objcopy
 objcopy = subprocess.Popen([
-  'objcopy', '--verbose',
-  '--add-section', f'.osrel=/dev/fd/{osrel_r}',
-  '--change-section-vma', '.osrel=0x0020000',
-  '--add-section', f'.cmdline=/dev/fd/{cmdline_r}',
-  '--change-section-vma', '.cmdline=0x0030000',
-  '--add-section', f'.linux={KERNEL}',
-  '--change-section-vma', '.linux=0x2000000',
-  '--add-section', f'.initrd=/dev/fd/{initramfs_r}',
-  '--change-section-vma', '.initrd=0x3000000',
-  EFISTUB, '/tmp/combined.efi',
-  ], pass_fds=[osrel_r, cmdline_r, initramfs_r])
+  "objcopy", "--verbose",
+  "--add-section", f".osrel=/dev/fd/{osrel_r}",
+  "--change-section-vma", ".osrel=0x0020000",
+  "--add-section", f".cmdline=/dev/fd/{cmdline_r}",
+  "--change-section-vma", ".cmdline=0x0030000",
+  "--add-section", f".linux={args.kernel}",
+  "--change-section-vma", ".linux=0x2000000",
+  "--add-section", f".initrd=/dev/fd/{initramfs_r}",
+  "--change-section-vma", ".initrd=0x3000000",
+  args.efistub, "/tmp/combined.efi"],
+  pass_fds=[osrel_r, cmdline_r, initramfs_r],
+)
 
 # wait for processes and close pipes
 # TODO: add timeout to wait and try:except to detect if objcopy crashes
-#print('join threads')
 cmdline_t.join()
 osrel_t.join()
-#print('wait for initramfs-cat')
-iniramfs_cat.wait()
+iniramfs_p.wait()
 initramfs_w.close()
-#print('wait for objcopy')
 objcopy.wait()
